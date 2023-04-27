18bde9e49dc3a0e5b2577d167a8b56864ca37216
doquangthang-zet <s3891873@rmit.edu.vn>
2023-04-27 14:24:47 +0700

    edit user api

--- src/dynamoDB.js
+++ src/dynamoDB.js
@@ -3,6 +3,8 @@ const groupAPI = 'https://sm6y72lnrl.execute-api.ap-southeast-1.amazonaws.com/ne
 const groupAPISpec = 'https://sm6y72lnrl.execute-api.ap-southeast-1.amazonaws.com/new_dev/groups/'
 const groupNUserAPI = ' https://1f4rcj5abe.execute-api.ap-southeast-1.amazonaws.com/dev/groupnuser'
 const groupNUserAPISpec = ' https://1f4rcj5abe.execute-api.ap-southeast-1.amazonaws.com/dev/groupnuser/'
+const userAPI = "https://7cgo7g7yy2.execute-api.ap-southeast-1.amazonaws.com/dev/users"
+const userAPISpec = "https://7cgo7g7yy2.execute-api.ap-southeast-1.amazonaws.com/dev/users/"
 //--------------CRUD Group------------------//
 //Mốt chỉnh lại cái lambda function đẻ thêm date với giờ vào nữa 
 const createGroup = async (data) => {

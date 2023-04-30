04dc9761800eee86c634af5e14fcbde52a6ff24b
sangnguyen0403 <sangnguyen04032002@gmail.com>
2023-04-30 21:40:10 +0700

    Finish reject request

--- src/components/notif_request/Notifications.js
+++ src/components/notif_request/Notifications.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Card, CardBody, Text, CardHeader, Heading, IconButton, VStack, Box, Spinner, Center } from "@chakra-ui/react"
+import { Card, CardBody, Text, CardHeader, Heading, IconButton, VStack, Box, Spinner, Center, useToast, Alert, Divider, Flex } from "@chakra-ui/react"
 import { BsCheck2 } from "react-icons/bs";
 import { IoMdClose } from "react-icons/io";
-import { createGroupNUser, getUserbyId, notifAPI, updateRequest } from '../../dynamoDB';
-import { useSelector } from 'react-redux';
+import { createGroupNUser, getGroupbyId, getUserbyId, notifAPI, updateRequest } from '../../dynamoDB';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectUser } from '../../redux/slices/userSlice';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { GROUP_ADD_MEMBER_SUCCESS, GROUP_DELETE_REQUEST_SUCCESS, GROUP_IDLE, addMemberToGroupAsync, changeGroupStatus, deleteRequestAsync, selectGroup } from '../../redux/slices/groupSlice';
 const STATUS_PENDING = 0;
 const STATUS_ACCEPTED = 1;
 const STATUS_REJECTED = 2;
@@ -14,9 +15,14 @@ const STATUS_REJECTED = 2;
 const Notifications = () => {
     const { user } = useSelector(selectUser)
     const [requestData, setRequestData] = useState([])
+    console.log(requestData)
+    const { status } = useSelector(selectGroup)
+    console.log("STATUS", status)
+    const dispatch = useDispatch()
     const [userDict, setUserDict] = useState({})
+    const [groupDict, setGroupDict] = useState({})
     const [loading, setLoading] = useState(true)
-
+    const toast = useToast()
 
     // Get request infos
     const fetchRequestData = async () => {
@@ -36,13 +42,16 @@ const Notifications = () => {
         const result = []
         try {
             const userData = await fetchSenderData(reflist)
+            const groupData = await fetchGroupData(reflist)
             for (let i = 0; i < reflist.length; i++) {
                 let senderData = userData[reflist[i].senderid]
-
+                let grpData = groupData[reflist[i].groupid]
+                console.log(grpData)
                 result.push({
                     id: reflist[i].id,
                     senderEmail: senderData.useremail,
                     senderName: senderData.username,
+                    groupname: grpData.groupname,
                     ...reflist[i]
                 })
             }
@@ -75,16 +84,68 @@ const Notifications = () => {
         return senderData
     }
 
+    const fetchGroupData = async (reflist) => {
+        const groupData = { ...groupDict }
+
+        for (let i = 0; i < reflist.length; i++) {
+            try {
+                let id = reflist[i].groupid
+                if (id in groupData) {
+                    continue
+                }
+
+                await getGroupbyId(id).then(item => {
+                    groupData[id] = item.Item
+                })
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        setGroupDict(groupData)
+        console.log("GROUP DATA", groupData)
+        return groupData
+    }
+
+    const errorMessage = (message) => {
+        Alert.alert(
+            "Fail",
+            message,
+            [{ text: "OK" }],
+            { cancelable: true }
+        );
+    }
+
 
     useEffect(() => {
         fetchRequestData()
     }, [])
 
+    useEffect(() => {
+        if (status === GROUP_ADD_MEMBER_SUCCESS) {
+            toast({
+                title: "Add New Member Successfully",
+                description: "Succesfully",
+                status: "success",
+                duration: 4000,
+                isClosable: true,
+            })
+            dispatch(changeGroupStatus(GROUP_IDLE))
+        } else if (status === GROUP_DELETE_REQUEST_SUCCESS) {
+            toast({
+                title: "Rejected Request Successfully",
+                description: "Succesfully",
+                status: "success",
+                duration: 4000,
+                isClosable: true,
+            })
+        }
+    }, [status])
+
 
     //Handle change status (Data = status + notifData)
     const handleChange = async (data) => {
-        const {id, groupid, receiverid, senderid} = data.data;
-        try{
+        const { id, groupid, receiverid, senderid } = data.data;
+        try {
             //update request status
             const newRequestInfo = {
                 id: id,
@@ -94,39 +155,55 @@ const Notifications = () => {
                 status: data.status
             }
             const fRequestInfo = JSON.stringify(newRequestInfo)
-    
-            updateRequest(fRequestInfo)
-            console.log(newRequestInfo)
-    
-            console.log("NHan")
-            //Add user to group
-            
-        }catch(e){
-        }
 
+            updateRequest(fRequestInfo)
+
+
+            //Add user to group
+            if (data.status === STATUS_ACCEPTED) {
+                const addMemberInfo = {
+                    groupid: groupid,
+                    role: "member",
+                    userid: senderid
+                }
+                dispatch(addMemberToGroupAsync(addMemberInfo))
+            } else if (data.status === STATUS_REJECTED) {
+                dispatch(deleteRequestAsync(id))
+            }
+        } catch (e) {
+            errorMessage(e.message)
+        }
     }
 
     return (
         <Box w='93%' mt='1em' p='1em' alignItems='left' ml="auto" mr="auto">
-            {loading ? 
-            <Center w='100' mt='10em'>
-                <Spinner
-                    thickness='4px'
-                    speed='0.65s'
-                    emptyColor='gray.200'
-                    color='blue.500'
-                    size='xl'
-                /> 
-            </Center>:
-                requestData.map((item, index) => (
+            <Heading
+                color="#A27083"
+                textAlign="center"
+                fontSize='2xl'
+            >
+                Notifications
+            </Heading>
+            {loading ?
+                <Center w='100' mt='10em'>
+                    <Spinner
+                        thickness='4px'
+                        speed='0.65s'
+                        emptyColor='gray.200'
+                        color='blue.500'
+                        size='xl'
+                    />
+                </Center> :
+                requestData.length === 0 ? <Flex w='100' mt='10'><Text>There is no notifications now</Text></Flex>
+                :requestData.map((item, index) => (
                     <Card key={index} m="2em" borderRadius='2xl' size="md" boxShadow='md' p='6' rounded='sm' >
                         <CardHeader>
                             <Heading size="sm" ml="0.5em">{item.senderName}</Heading>
                         </CardHeader>
                         <CardBody mt="-1em">
-                            <Text ml="0.5em">{item.senderEmail} wanted to join your group!
-                                <IconButton float="right" mr="1em" mt="-1.25em" variant="ghost" icon={<IoMdClose size="2em" color="#E48181" />} onClick={() => handleChange({status: STATUS_REJECTED, data: item})} />
-                                <IconButton float={"right"} mt="-1.25em" variant="ghost" icon={<BsCheck2 float="right" size="2em" color="#306643" />} onClick={() => handleChange({status: STATUS_ACCEPTED, data: item})} />
+                            <Text ml="0.5em">{item.senderEmail} wanted to join your group {item.groupname}!
+                                <IconButton float="right" mr="1em" mt="-1.25em" variant="ghost" icon={<IoMdClose size="2em" color="#E48181" />} onClick={() => handleChange({ status: STATUS_REJECTED, data: item })} />
+                                <IconButton float={"right"} mt="-1.25em" variant="ghost" icon={<BsCheck2 float="right" size="2em" color="#306643" />} onClick={() => handleChange({ status: STATUS_ACCEPTED, data: item })} />
                             </Text>
                         </CardBody>
                     </Card>
--- src/redux/slices/groupSlice.js
+++ src/redux/slices/groupSlice.js
@@ -1,10 +1,17 @@
 import { createSlice, createAsyncThunk, nanoid } from "@reduxjs/toolkit";
-import { createRequest } from "../../dynamoDB";
+import { createGroupNUser, createRequest } from "../../dynamoDB";
+import { deleteRequest } from "../../dynamoDB";
 
 export const GROUP_IDLE = 0
 export const GROUP_REQUEST_PENDING = 1
 export const GROUP_REQUEST_REJECTED = 2
 export const GROUP_REQUEST_SUCCESS = 3
+export const GROUP_ADD_MEMBER_PENDING = 4
+export const GROUP_ADD_MEMBER_SUCCESS = 5
+export const GROUP_ADD_MEMBER_REJECTED = 6
+export const GROUP_DELETE_REQUEST_PENDING = 7
+export const GROUP_DELETE_REQUEST_SUCCESS = 8
+export const GROUP_DELETE_REQUEST_REJECTED = 9
 
 const initialState = {
     groupInfo:{
@@ -27,11 +34,29 @@ export const createGroupRequestAsync = createAsyncThunk('group/createGroupReques
     await createRequest(final_requestInfo)
 })
 
+export const addMemberToGroupAsync = createAsyncThunk('group/addMemberToGroupAsync', async(data) => {
+    const hash = nanoid()
+    const memberInfo = {
+        "id" : hash,
+        ...data
+    }
+    const fMemberInfo = JSON.stringify(memberInfo)
+
+    await createGroupNUser(fMemberInfo)
+
+})
+
+export const deleteRequestAsync = createAsyncThunk('group/deleteRequestAsync', async(id) => {
+    await deleteRequest(id)
+})
+
 export const groupSlice = createSlice({
     name: 'group',
     initialState,
     reducers: {
-        
+        changeGroupStatus: (state, action) => {
+			state.status = action.payload
+		},
     },
     extraReducers: builder => {
         builder
@@ -44,11 +69,29 @@ export const groupSlice = createSlice({
             .addCase(createGroupRequestAsync.rejected,(state,action) => {
                 state.status = GROUP_REQUEST_REJECTED
             })
+            .addCase(addMemberToGroupAsync.pending, state => {
+                state.status = GROUP_ADD_MEMBER_PENDING
+            })
+            .addCase(addMemberToGroupAsync.fulfilled, (state,action) => {
+                state.status = GROUP_ADD_MEMBER_SUCCESS
+            })
+            .addCase(addMemberToGroupAsync.rejected, (state, action) => {
+                state.status = GROUP_ADD_MEMBER_REJECTED
+            })
+            .addCase(deleteRequestAsync.pending, state => {
+                state.status = GROUP_DELETE_REQUEST_PENDING
+            })
+            .addCase(deleteRequestAsync.fulfilled, state => {
+                state.status = GROUP_DELETE_REQUEST_SUCCESS
+            })
+            .addCase(deleteRequestAsync.rejected, state => {
+                state.status = GROUP_DELETE_REQUEST_REJECTED
+            })
     }
 })
 
 export const selectGroup = (state) => state.group
 
-export const {  } = groupSlice.actions
+export const { changeGroupStatus } = groupSlice.actions
 
 export default groupSlice.reducer
